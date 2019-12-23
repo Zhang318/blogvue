b990ed64dd1f2606dc8ba62de7f6848467c9bbbf
张鑫 <318328668@qq.com>
2019-12-23 20:39:20 +0800

    zx

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.prototype.$axios = axios
+Vue.config.productionTip = false
+Vue.use(ElementUi)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
