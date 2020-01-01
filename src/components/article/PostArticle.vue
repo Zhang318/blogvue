836cea7ef7a02549c721a1f51808a0b1a80d7883
张鑫 <318328668@qq.com>
2020-01-02 07:33:17 +0800

    架构

--- src/components/article/PostArticle.vue
+++ src/components/article/PostArticle.vue
@@ -1,17 +1,162 @@
 <template>
-  <mavon-editor></mavon-editor>
+  <el-container v-loading="loading" class="post-article">
+    <el-header class="header">
+      <el-select v-model="article.classifyId" placeholder="请选择文章栏目" style="width: 150px;">
+        <el-option
+          v-for="item in categories"
+          :key="item.classifyId"
+          :label="item.classifyName"
+          :value="item.classifyId">
+        </el-option>
+      </el-select>
+      <el-input v-model="article.title" placeholder="请输入标题..." style="width: 400px;margin-left: 10px"></el-input>
+      <el-tag
+        :key="tag"
+        v-for="tag in article.dynamicTags"
+        closable
+        :disable-transitions="false"
+        @close="handleClose(tag)" style="margin-left: 10px">
+        {{tag}}
+      </el-tag>
+      <el-input
+        class="input-new-tag"
+        v-if="tagInputVisible"
+        v-model="tagValue"
+        ref="saveTagInput"
+        size="small"
+        @keyup.enter.native="handleInputConfirm"
+        @blur="handleInputConfirm">
+      </el-input>
+      <el-button v-else class="button-new-tag" type="primary" size="small" @click="showInput">+Tag</el-button>
+    </el-header>
+    <el-main class="main">
+      <div id="editor">
+        <mavon-editor style="height: 100%;width: 100%;" ref=md @imgAdd="imgAdd"
+                      @imgDel="imgDel" v-model="article.mdContent"></mavon-editor>
+      </div>
+      <div style="display: flex;align-items: center;margin-top: 15px;justify-content: flex-end">
+        <el-button @click="cancelEdit" v-if="from!=undefined">放弃修改</el-button>
+        <template v-if="from==undefined || from=='draft'">
+          <el-button @click="saveBlog(0)">保存到草稿箱</el-button>
+          <el-button type="primary" @click="saveBlog(1)">发表文章</el-button>
+        </template>
+        <template v-else="from==post">
+          <el-button type="primary" @click="saveBlog(1)">保存修改</el-button>
+        </template>
+      </div>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
+  import {postRequest} from '../../utils/api'
+  import {getRequest} from '../../utils/api'
+  import {uploadFileRequest} from '../../utils/api'
+  // Local Registration
   import {mavonEditor} from 'mavon-editor'
   // 可以通过 mavonEditor.markdownIt 获取解析器markdown-it对象
   import 'mavon-editor/dist/css/index.css'
+  import {isNotNullORBlank} from '../../utils/utils'
 
   export default {
     components:{
       mavonEditor
     },
-    name: "postArticle"
+    name: "postArticle",
+    data() {
+      return {
+        categories: [],
+        tagInputVisible: false,
+        tagValue: '',
+        loading: false,
+        from: '',
+        article: {
+          id: '-1',
+          dynamicTags: [],
+          title: '',
+          mdContent: '',
+          classifyId: ''
+        }
+      }
+    },
+    methods:{
+      cancelEdit(){
+        this.$router.go(-1)
+      },
+      saveBlog(state){
+        if (!(isNotNullORBlank(this.article.title, this.article.mdContent, this.article.cid))) {
+          this.$message({type: 'error', message: '数据不能为空!'});
+          return;
+        }
+        var _this = this;
+        _this.loading = true;
+        postRequest("/article/add", {
+          id: _this.article.id,
+          title: _this.article.title,
+          mdContent: _this.article.mdContent,
+          htmlContent: _this.$refs.md.d_render,
+          cid: _this.article.cid,
+          state: state,
+          dynamicTags: _this.article.dynamicTags
+        }).then(resp=> {
+          _this.loading = false;
+          if (resp.status == 200 && resp.data.status == 'success') {
+            _this.article.id = resp.data.msg;
+            _this.$message({type: 'success', message: state == 0 ? '保存成功!' : '发布成功!'});
+//            if (_this.from != undefined) {
+            window.bus.$emit('blogTableReload')
+//            }
+            if (state == 1) {
+              _this.$router.replace({path: '/articleList'});
+            }
+          }
+        }, resp=> {
+          _this.loading = false;
+          _this.$message({type: 'error', message: state == 0 ? '保存草稿失败!' : '博客发布失败!'});
+        })
+      },
+      imgAdd(pos, $file){
+        var _this = this;
+        // 第一步.将图片上传到服务器.
+        var formdata = new FormData();
+        formdata.append('image', $file);
+        uploadFileRequest("/article/uploadimg", formdata).then(resp=> {
+          var json = resp.data;
+          if (json.status == 'success') {
+//            _this.$refs.md.$imgUpdateByUrl(pos, json.msg)
+            _this.$refs.md.$imglst2Url([[pos, json.msg]])
+          } else {
+            _this.$message({type: json.status, message: json.msg});
+          }
+        });
+      },
+      imgDel(pos){
+
+      },
+      getCategories(){
+        let _this = this;
+        getRequest("/calssify/getAll").then(resp=> {
+          _this.categories = resp.data.data;
+        });
+      },
+      handleClose(tag) {
+        this.article.dynamicTags.splice(this.article.dynamicTags.indexOf(tag), 1);
+      },
+      showInput() {
+        this.tagInputVisible = true;
+        this.$nextTick(_ => {
+          this.$refs.saveTagInput.$refs.input.focus();
+        });
+      },
+      handleInputConfirm() {
+        let tagValue = this.tagValue;
+        if (tagValue) {
+          this.article.dynamicTags.push(tagValue);
+        }
+        this.tagInputVisible = false;
+        this.tagValue = '';
+      }
+    }
   }
 </script>
 
